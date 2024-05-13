39e0c344df2dfdd5d480ea498afb8f9b39acb061
minhoshin11 <tlsalsgh777@naver.com>
2024-05-13 13:10:32 +0900

    Chore: git-ignore 이슈
    최상단에 git-ignore 추가

--- front/shared/api/axios.ts
+++ front/shared/api/axios.ts
@@ -5,22 +5,3 @@ export const instance = axios.create({
   timeout: 40000,
   withCredentials: true,
 })
-
-//사용 방법 : 해당 페이지에서 const formData = {username, password} 등 써야하는 form 만들기
-//apiPost('api/login', formData)
-// export const apiPost = async (
-//   url: string,
-//   formData: any,
-//   headers?: AxiosRequestConfig["headers"],
-// ) => {
-//   try {
-//     const config: AxiosRequestConfig = { data: formData }
-//     if (headers !== undefined) {
-//       config.headers = headers
-//     }
-//     const response = await instance.post(url, formData, config)
-//     return response.data
-//   } catch (error) {
-//     console.log("에러임")
-//   }
-// }
