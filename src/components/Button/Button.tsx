8002d65bd22d8ce40635988e08b7c311bc833fd5
chaayee0123 <chayee0614@gmail.com>
2024-05-15 00:17:53 +0900

    Feat: signup page 구현
    회원가입 페이지 구현, api연결 성공

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import styles from "./Button.module.css"
+import React from "react";
+import styles from "./Button.module.css";
 
-type ButtonSize = "sm" | "md" | "lg"
-type ButtonVariant = "primary" | "secondary"
+type ButtonSize = "sm" | "md" | "lg";
+type ButtonVariant = "primary" | "secondary";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  size: ButtonSize
-  variant: ButtonVariant
+  size: ButtonSize;
+  variant: ButtonVariant;
 }
 
 const Button = ({ size, variant, children, ...props }: ButtonProps) => {
-  const sizeClass = styles[`size${size}`]
-  const variantClass = styles[`variant${variant}`]
-  const disabledClass = props.disabled ? styles.buttondisabled : ""
+  const sizeClass = styles[`size${size}`];
+  const variantClass = styles[`variant${variant}`];
+  const disabledClass = props.disabled ? styles.buttondisabled : "";
 
   return (
     <button
@@ -21,7 +21,7 @@ const Button = ({ size, variant, children, ...props }: ButtonProps) => {
     >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
