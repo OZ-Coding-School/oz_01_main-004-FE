fece4987c43b5287b51d774dc01560489699e420
kshenk00 <00byoon@gmail.com>
2024-05-31 00:29:11 +0900

    Feat: likelikst 페이지네이션 추가
    likelist 수정된 api적용과 페이지네이션 추가

--- src/pages/mypage /likelist/likelist.tsx
+++ src/pages/mypage /likelist/likelist.tsx
@@ -6,27 +6,37 @@ import instance from "../../../api/axios";
 import { Recipe } from "../../../components/mealgrid/type";
 import Postcard from "../../../components/postcard/postcard";
 import useFavoriteStatus from "../../../hooks/use_favoriestatus";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const LikeList: React.FC = () => {
   const [likeList, setLikeList] = useState<Recipe[]>([]);
   const { favoriteStates } = useFavoriteStatus();
-  const fetchLikeList = async () => {
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchLikeList = async (page: number) => {
     try {
       const response = await instance.get("/favorite/list/", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access")}`,
         },
+        params: {
+          page: page,
+        },
       });
 
+      console.log(response.data, "응답확인");
       const data = response.data;
 
-      if (data && data.favorite_list) {
-        const updatedLikeList = data.favorite_list.map((innerArray: any) => ({
-          ...innerArray.recipe,
+      if (data && data.results) {
+        const updatedLikeList = data.results.map((item: any) => ({
+          ...item.recipe,
           is_favorite: true,
         }));
 
         setLikeList(updatedLikeList);
+        console.log(data.results, "여기favorite_list");
+        setTotalPages(Math.ceil(response.data.count / 12));
       }
     } catch (error) {
       console.error("Error fetching like list:", error);
@@ -34,8 +44,24 @@ const LikeList: React.FC = () => {
   };
 
   useEffect(() => {
-    fetchLikeList();
-  }, []);
+    fetchLikeList(page);
+  }, [page]);
+
+  const handleNextPage = () => {
+    if (page < totalPages) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage((prevPage) => prevPage - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    setPage(pageNumber);
+  };
 
   return (
     <div className={styles.container}>
@@ -49,6 +75,34 @@ const LikeList: React.FC = () => {
             />
           ))}
         </PostGrid>
+
+        <div className={styles.ButtonContainer}>
+          <button
+            className={styles.MoveBtn}
+            onClick={handlePreviousPage}
+            disabled={page <= 1}
+          >
+            <IoIosArrowBack color="#fff" />
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handlePageClick(index + 1)}
+              className={
+                index + 1 === page ? styles.activePageButton : styles.Button
+              }
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            className={styles.MoveBtn}
+            onClick={handleNextPage}
+            disabled={page >= totalPages}
+          >
+            <IoIosArrowForward color="#fff" />
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -58,7 +112,7 @@ const PostGrid = styled.div`
   display: flex;
   flex-wrap: wrap;
   gap: 28px;
-  justify-content: flex-start;
+  justify-content: center;
 `;
 
 export default LikeList;
